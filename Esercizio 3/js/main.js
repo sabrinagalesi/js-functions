a39fb3e9e8441180de7c4a28f43042a18fa0def2
sabrinagalesi <sabrinagalesi8@gmail.com>
2020-04-20 12:10:55 +0200

    Esercizio 3 completato e iniziato il 4

--- Esercizio 3/js/main.js
+++ Esercizio 3/js/main.js
@@ -1,2 +1,30 @@
 // (3) Una funzione per capire se la parola è palindroma 
 
+/* 1 faccio un prompt in cui chiedo di scrivere una parola 
+   2 faccio una funzione con dentro un for che mi conta le posizioni della parola in ordine decrescente
+   3 if for è uguale alla parola digitata, allora è palindroma*/
+
+var parola = prompt("Digita una parola. Ti dirò se è palindroma!");
+
+console.log(parola);
+
+var parolaInvertita = "";
+
+function calcoloParola(parolaInvertita) {
+    for (counter = parola.length - 1; counter >= 0; counter--) {
+        parolaInvertita += parola[counter];
+        console.log(parola[counter], counter, parolaInvertita);
+    } 
+    return parolaInvertita;
+}
+
+parolaInvertita = calcoloParola(parolaInvertita);
+
+console.log(parolaInvertita);
+
+if(parola == parolaInvertita) {
+    alert("Palindroma!");
+} else {
+    alert("Non è palindroma!");
+}
+
--- Esercizio 4/js/main.js
+++ Esercizio 4/js/main.js
@@ -1,6 +1,17 @@
 // (4) L’utente sceglie pari o dispari e un numero  da 1 a 5.  Generiamo un numero random (sempre da 1 a 5) per il computer.  
 // Sommiamo i due numeri e dichiariamo chi ha vinto. 
 
-/* Prima cosa fare un prompt per chiedere un numero da 1 a 5
+/* Prima cosa fare un prompt per chiedere un numero da 1 a 5 e uno per chiedere di scegliere tra pari e dispari
    Seconda cosa, generiamo un numero random per il pc
-   Terza cosa (chiedere a Nik in che senso sommare)*/
+   Terza cosa sommare i due numeri, in base a cosa ha scelto l'utente, se è pari ha vinto/perso e idem se è dispari */
+
+var numero = parseInt(prompt("Scegli un numero da 1 a 5!"));
+var pariDispari = prompt("Scegli: pari o dispari?");
+var numeroPc = Math.floor(Math.random() * 5) + 1;
+
+console.log(numero, pariDispari, numeroPc);
+
+var somma = numero + numeroPc;
+
+console.log(somma);
+
